0cf03f4a480436abc3422ce59529102205e1663e
Art <artemgoryunov89@gmail.com>
2023-07-31 14:53:40 +0400

    Программа для вычисления суммы цифр

---
+++ Exercise-27/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+
+int result = Summ(number);
+Console.WriteLine($"Сумма цифр числа равна {result}");
+
+int Summ(int num)
+{
+    while (num > 0)
+    {
+        summ += num % 10;
+        num = num / 10;
+    }
+    return summ;
+}
+
